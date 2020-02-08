e18dd2f78fbbd0db63db078a9065a918088291b7
Luiz Fernando Rodrigues Sanches <deploy@travis-ci.org>
2020-02-08 14:18:11 +0000

    Deploy luizzzdev/burger-builder to github.com/luizzzdev/burger-builder.git:gh-pages

---
+++ precache-manifest.0e65e05f4ab216b2bde4e49b4fe396a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64ca9e762b6a240e048dca6954b06d06",
+    "url": "/burger-builder/index.html"
+  },
+  {
+    "revision": "fbb8dc99e5a77f0ea1a4",
+    "url": "/burger-builder/static/css/main.4cab4b99.chunk.css"
+  },
+  {
+    "revision": "ffceb3cd90ef7329c905",
+    "url": "/burger-builder/static/js/2.6aeeb001.chunk.js"
+  },
+  {
+    "revision": "fbb8dc99e5a77f0ea1a4",
+    "url": "/burger-builder/static/js/main.9e847d8a.chunk.js"
+  },
+  {
+    "revision": "3b6782a9d646397567d7",
+    "url": "/burger-builder/static/js/runtime-main.9b0b8f10.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-builder/static/media/burger-logo.b8503d26.png"
+  }
+]);
